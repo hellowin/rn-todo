df7ce02ba15e21be80bccee6c82de65030be222e
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-05-27 12:41:20 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base'
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Badge } from 'native-base'
 import { Platform, StatusBar } from 'react-native'
 import TodoPage from 'src/domain/todo/container/TodoPage'
 
@@ -20,6 +20,19 @@ class App extends React.Component {
         <Content>
           <TodoPage />
         </Content>
+        <Footer >
+          <FooterTab>
+            <Button badge vertical>
+              <Badge><Text>2</Text></Badge>
+              <Icon name="list" />
+              <Text>Active</Text>
+            </Button>
+            <Button>
+              <Icon name="checkbox" />
+              <Text>Done</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     );
   }
